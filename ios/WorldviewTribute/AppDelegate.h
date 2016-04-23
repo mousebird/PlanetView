854d8efa92e2403f366debebf78637791b791edc
Steve Gifford <sjg@mousebirdconsulting.com>
2016-04-23 11:23:11 -0700

    Very first checkin.  Haven't added Pods yet.

---
+++ ios/WorldviewTribute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldviewTribute
+//
+//  Created by Steve Gifford on 4/23/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/WorldviewTribute/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorldviewTribute
+//
+//  Created by Steve Gifford on 4/23/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

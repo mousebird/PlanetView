16f79cfc5f6ef35734e46981996884ee988d2c6c
Alli Dryer <awdryer@gmail.com>
2016-04-23 20:34:24 -0700

    Animated the menu sliding in and out

--- ios/LayerManagerViewController.swift
+++ ios/LayerManagerViewController.swift
@@ -13,6 +13,8 @@ class LayerManagerViewController: UIViewController {
     @IBOutlet weak var viewLayersBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var layerManagerView: UIView!
     @IBOutlet weak var datePickerView: UIView!
+    @IBOutlet weak var leftDateButton: UIButton!
+    @IBOutlet weak var rightDateButton: UIButton!
     
     var globeViewC = GlobeViewController()
     var config = WVTConfig()
@@ -36,9 +38,8 @@ class LayerManagerViewController: UIViewController {
         layerManagerView.layer.cornerRadius = 10
         datePickerView.layer.cornerRadius = 10
         
-        // Style the bar button items
-        viewLayersBarButtonItem.setTitleTextAttributes([ NSFontAttributeName: UIFont(name: "Avenir-heavy", size: 14)!], forState: UIControlState.Normal)
-
+        // Style the bar button item
+        viewLayersBarButtonItem.setTitleTextAttributes([ NSFontAttributeName:UIFont(name: "Avenir-heavy", size: 14)!], forState: UIControlState.Normal)
         
         // Configuration file for the UI
         let configName = NSBundle.mainBundle().pathForResource("wv", ofType: "json")
@@ -49,7 +50,27 @@ class LayerManagerViewController: UIViewController {
     }
     
     @IBAction func onViewLayersBarButtonItemPressed(sender: AnyObject) {
-        layerManagerView.alpha = 1
+        if layerManagerView.frame.origin.y == 76 {
+            self.viewLayersBarButtonItem.title = "View Layers"
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                self.layerManagerView.transform = CGAffineTransformMakeTranslation(0.0, -800.0)
+                self.datePickerView.transform = CGAffineTransformMakeTranslation(0.0, -800.0)
+                self.layerManagerView.alpha = 0
+                self.datePickerView.alpha = 0
+                }, completion: { (finished: Bool) in
+                //
+            })
+        } else {
+            self.viewLayersBarButtonItem.title = "Hide Layers"
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+                self.layerManagerView.transform = CGAffineTransformMakeTranslation(0.0, 0.0)
+                self.datePickerView.transform = CGAffineTransformMakeTranslation(0.0, 0.0)
+                self.layerManagerView.alpha = 1
+                self.datePickerView.alpha = 1
+                }, completion: { (finished: Bool) in
+                    //
+            })
+        }
     }
     
     // turn the status bar white
